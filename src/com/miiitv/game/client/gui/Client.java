35355c0215afdcb2bf03057d188a9294bfa41ac8
Ting Cheng <ting_cheng@miiicasa.com>
2013-10-26 16:26:49 +0800

    1. add Client activity and layout 2. add login icon

--- src/com/miiitv/game/client/gui/Client.java
+++ src/com/miiitv/game/client/gui/Client.java
@@ -1,23 +1,46 @@
 package com.miiitv.game.client.gui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
+import com.miiitv.game.client.Logger;
 import com.miiitv.game.client.R;
 
-public class Client extends Activity {
+public class Client extends Activity implements OnClickListener {
+	
+	private final static String TAG = "Client";
+	private Context mContext = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.client);
+		mContext = this;
+		((ImageView) findViewById(R.id.client_login)).setOnClickListener(this);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Logger.i(TAG, "onResume");
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Logger.i(TAG, "onDestory");
+		mContext = null;
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.client_login:
+				break;
+		}
+	}	
 }
