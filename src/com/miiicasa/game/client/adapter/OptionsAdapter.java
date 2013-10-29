82483d1a23c6cb221dd546797803e9691fa22e32
Ting Cheng <ting_cheng@miiicasa.com>
2013-10-30 02:20:30 +0800

    1. add options adapter

---
+++ src/com/miiicasa/game/client/adapter/OptionsAdapter.java
@@ -0,0 +1,65 @@
+package com.miiicasa.game.client.adapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.miiitv.game.client.R;
+
+public class OptionsAdapter extends BaseAdapter {
+
+	private final static String TAG = "OptionsAdapter";
+	private LayoutInflater inflater = null;
+	private JSONArray optionsJSON = null;
+	
+	public class ViewHolder {
+		
+		public TextView answerTextView = null;
+	}
+	
+	
+	public OptionsAdapter(Context context, JSONArray optionsJSON) {
+		this.optionsJSON = optionsJSON;
+		inflater = LayoutInflater.from(context);
+	}
+	
+	@Override
+	public int getCount() {
+		return optionsJSON.length();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder = null;
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.answer, parent, false);
+			holder = new ViewHolder();
+			holder.answerTextView = (TextView) convertView.findViewById(R.id.answer);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		try {
+			holder.answerTextView.setText(optionsJSON.getString(position));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return convertView;
+	}
+}
--- src/com/miiitv/game/client/gui/StartActivity.java
+++ src/com/miiitv/game/client/gui/StartActivity.java
@@ -3,10 +3,15 @@ package com.miiitv.game.client.gui;
 import org.json.JSONArray;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.miiicasa.game.client.adapter.OptionsAdapter;
 import com.miiitv.game.client.App;
 import com.miiitv.game.client.Logger;
 import com.miiitv.game.client.R;
@@ -15,6 +20,11 @@ public class StartActivity extends Activity implements StartListener, ConnectLis
 	
 	private final static String TAG = "StartActivity";
 	private Context mContext = null;
+	private GridLayout grid = null;
+	private RelativeLayout layout = null;
+	private OptionsAdapter adapter = null;
+	private ProgressDialog loading = null;
+	private JSONArray optionsJSON = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,27 @@ public class StartActivity extends Activity implements StartListener, ConnectLis
 		Logger.i(TAG, "onCreate");
 		setContentView(R.layout.start);
 		mContext = this;
+		grid = (GridLayout) findViewById(R.id.start_list);
+		layout = (RelativeLayout) findViewById(R.id.start);
+	}
+	
+	private void startShock() {
+		layout.setBackground(getResources().getDrawable(R.drawable.choice_bg));
+		grid.setVisibility(View.GONE);
+	}
+	
+	private void stopShock() {
+		layout.setBackground(getResources().getDrawable(R.drawable.bell_bg));
+		grid.setVisibility(View.VISIBLE);
+	}
+	
+	private void waitLoad() {
+		loading = new ProgressDialog(mContext);
+		loading.setTitle(R.string.start_wait_title);
+		loading.setMessage(getString(R.string.start_wait_message));
+		loading.setCancelable(false);
+		loading.setCanceledOnTouchOutside(false);
+		loading.show();
 	}
 	
 	@Override
@@ -42,7 +73,7 @@ public class StartActivity extends Activity implements StartListener, ConnectLis
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
 			App.getInstance().clientService.disConnectServer();
-			finish();
+			onFail();
 		}
 		return super.onKeyDown(keyCode, event);
 	}
@@ -56,12 +87,12 @@ public class StartActivity extends Activity implements StartListener, ConnectLis
 
 	@Override
 	public void start(JSONArray options) {
-		// TODO Auto-generated method stub
+		optionsJSON = options;
 	}
 
 	@Override
 	public void lock() {
-		// TODO Auto-generated method stub
+		startShock();
 	}
 
 	@Override
